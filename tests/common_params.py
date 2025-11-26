bbc9a182c0264f22278fedac42f2e813babd2f37
Ivo Maceira <ivomaceira@gmail.com>
2025-11-26 01:27:15 +0100

    test: add API tests

---
+++ tests/common_params.py
@@ -0,0 +1,50 @@
+PARAM_SETS = [
+    {
+        "num_players": 6,
+        "num_players_alive": 6,
+        "deck": {
+            "Ambassador": 3,
+            "Assassin": 3,
+            "Captain": 3,
+            "Contessa": 3,
+            "Duke": 3,
+        },
+        "dead_draw": False,
+    },
+    {
+        "num_players": 6,
+        "num_players_alive": 2,
+        "deck": {
+            "Ambassador": 3,
+            "Assassin": 3,
+            "Captain": 3,
+            "Contessa": 3,
+            "Duke": 3,
+        },
+        "dead_draw": True,
+    },
+    {
+        "num_players": 2,
+        "num_players_alive": 2,
+        "deck": {
+            "Ambassador": 2,
+            "Assassin": 2,
+            "Captain": 2,
+            "Contessa": 2,
+            "Duke": 2,
+        },
+        "dead_draw": False,
+    },
+    {
+        "num_players": 3,
+        "num_players_alive": 2,
+        "deck": {
+            "Ambassador": 3,
+            "Assassin": 3,
+            "Captain": 3,
+            "Contessa": 3,
+            "Duke": 3,
+        },
+        "dead_draw": True,
+    },
+]
